ce9ed3d79888893a516aa8f5aa83762017c0ee90
André Weller <info@lost.design>
2023-09-20 14:54:41 +0200

    refactor: use env instead of hardcoded urls

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -2,6 +2,7 @@ import type { UseFetchOptions} from 'nuxt/app';
 import {useRequestHeaders} from "nuxt/app";
 
 export const useApiFetch = <T>(path: string, options: UseFetchOptions<T> = {}) => {
+  const config = useRuntimeConfig();
   const token = useCookie('XSRF-TOKEN');
   let headers: any = {
     'Accept': 'application/json',
@@ -16,12 +17,15 @@ export const useApiFetch = <T>(path: string, options: UseFetchOptions<T> = {}) =
       ...headers,
       ...useRequestHeaders(["cookie"]),
       // TODO: use env values instead!
-      referer: "http://localhost:3000",
+      referer: config.public.referrer,
       'Accept': "application/json",
     }
   }
 
-  return useFetch('http://localhost:8000' + path, {
+  console.log(config.public.tempAuthUrl ?? config.public.apiUrl,
+    config.public.tempAuthUrl, config.public.apiUrl)
+
+  return useFetch(`${config.public.tempAuthUrl ?? config.public.apiUrl}${path}`, {
     credentials: "include",
     watch: false,
     ...options,
