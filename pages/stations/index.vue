a236b7222bd099d1c446a780bc8cd577ce80e1f7
André Weller <info@lost.design>
2023-09-20 20:47:04 +0200

    feat: add stations empty state

--- pages/stations/index.vue
+++ pages/stations/index.vue
@@ -12,7 +12,14 @@ watch(() => route.query.page, () => {
 
 <template>
   <ContentWrapper>
-    <List :stations="store.stations"/>
+    <List v-if="store.stations.length" :stations="store.stations"/>
+    <template v-if="!store.stations.length">
+      <div class="text-center py-56">
+        <IconsTruck class="mx-auto h-12 w-12 text-red-500"/>
+        <h3 class="mt-2 text-2xl font-semibold text-gray-900">Kein Ergebnis</h3>
+        <p class="mt-4 text-sm text-gray-500">Es sind aktuelle keine Wachen hinterlegt.<br>Demnächst wird es möglich sein neue Wachen einzutragen!</p>
+      </div>
+    </template>
     <Paginator class="my-12" :paginator="store.stationsPaginator" />
   </ContentWrapper>
 </template>
