2619fe76c7e69c6db920eb6fbe83cdcebc5e5adb
André Weller <info@lost.design>
2023-09-21 16:20:32 +0200

    fix(Banner): adjust spacing

--- components/Banner.vue
+++ components/Banner.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="sticky top-0 bg-red-600 gap-x-6 px-12 py-8 z-50">
+  <div class="sticky top-0 bg-red-600 gap-x-6 px-6 py-8 z-50">
     <p class="text-sm font-bold text-white">
        <IconsAlertFilled class="inline h-6 w-6 mr-2"/> Die Website ist im Aufbau! <span class="font-normal">Trete der Community bei, um die neuesten Updates zu erhalten und aktiv mitzugestalten.</span> <a href="https://discord.gg/6mRPXyCamB" target="_blank" class="underline hover:text-black">Zur Community!</a>
     </p>
