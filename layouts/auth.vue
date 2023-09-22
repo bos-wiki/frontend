5c0ca97b61f94b8f67ff1e1f6a0a140406b02d1c
André Weller <info@lost.design>
2023-09-22 13:06:53 +0200

    feat: add link to auth logo

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex min-h-full h-screen">
     <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-12 xl:px-18">
-      <header class="flex flex-col justify-center items-center space-y-4 mb-12">
-        <IconsBroadcast class="w-12 h-12 text-black" />
-        <h1 class="text-4xl font-bold">bos.wiki</h1>
-      </header>
+      <NuxtLink to="/" class="hover:text-red-500 hover:cursor-pointer mb-12">
+        <header class="flex flex-col justify-center items-center space-y-4">
+          <IconsBroadcast class="w-12 h-12" />
+          <h1 class="text-4xl font-bold">bos.wiki</h1>
+        </header>
+      </NuxtLink>
       <NuxtPage class="mx-auto w-full max-w-sm lg:w-96"/>
       <Banner class="mx-auto w-full max-w-sm lg:w-96 mt-12">
         <IconsAlertFilled class="inline h-6 w-6 mr-2"/>
