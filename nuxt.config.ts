9193a7b258a9f11f30f69deb68247eee03474f2f
André Weller <info@lost.design>
2023-09-22 14:51:43 +0200

    fix: add main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL,
