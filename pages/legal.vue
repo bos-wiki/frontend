2a0ebdb2f9b6eba4ef74586beb62902dfcc22683
André Weller <info@lost.design>
2023-10-13 17:33:22 +0200

    feat: add legal

---
+++ pages/legal.vue
@@ -0,0 +1,31 @@
+<template>
+  <ContentWrapper class="my-12 prose-sm">
+    <h1>Impressum</h1>
+    <h2>Diensteanbieter</h2>
+
+    <p>André Weller<br>c/o autorenglück.de<br>Franz-Mehring-Str. 15<br>01237 Dresden</p>
+
+    <h2>Kontaktmöglichkeiten</h2>
+    <p><a href="mailto:social@bos.wiki">social@bos.wiki</a></p>
+
+    <h2>Journalistisch-redaktionelle Angebote</h2>
+    <p>Inhaltlich verantwortlich: André Weller</p>
+
+    <h2>Audiovisuelle Mediendienste</h2>
+    <p>Sitzland: Deutschland. Zuständige Regulierungs-, bzw. Aufsichtsbehörde: Bayerische Landeszentrale für neue Medien (BLM), https://www.blm.de/</p>
+
+    <h2>Social Media und andere Onlinepräsenzen</h2>
+    <p>Dieses Impressum gilt auch für die folgenden Social-Media-Präsenzen und Onlineprofile:</p>
+    <ul class="list-disc">
+      <li><a href="https://twitter.com/boswiki/">twitter.com/boswiki</a></li>
+    </ul>
+
+    <h2>Haftungs- und Schutzrechtshinweise</h2>
+    <p>Haftungsausschluss: Die Inhalte dieses Onlineangebotes wurden sorgfältig und nach unserem aktuellen Kenntnisstand erstellt, dienen jedoch nur der Information und entfalten keine rechtlich bindende Wirkung, sofern es sich nicht um gesetzlich verpflichtende Informationen (z. B. das Impressum, die Datenschutzerklärung, AGB oder verpflichtende Belehrungen von Verbrauchern) handelt. Wir behalten uns vor, die Inhalte vollständig oder teilweise zu ändern oder zu löschen, soweit vertragliche Verpflichtungen unberührt bleiben. Alle Angebote sind freibleibend und unverbindlich.</p>
+    <p>Links auf fremde Webseiten: Die Inhalte fremder Webseiten, auf die wir direkt oder indirekt verweisen, liegen außerhalb unseres Verantwortungsbereiches und wir machen sie uns nicht zu Eigen. Für alle Inhalte und Nachteile, die aus der Nutzung der in den verlinkten Webseiten aufrufbaren Informationen entstehen, übernehmen wir keine Verantwortung.</p>
+    <p>Urheberrechte und Markenrechte: Alle auf dieser Website dargestellten Inhalte, wie Texte, Fotografien, Grafiken, Marken und Warenzeichen sind durch die jeweiligen Schutzrechte (Urheberrechte, Markenrechte) geschützt. Die Verwendung, Vervielfältigung usw. unterliegen unseren Rechten oder den Rechten der jeweiligen Urheber bzw. Rechteinhaber.</p>
+    <p>Hinweise auf Rechtsverstöße: Sollten Sie innerhalb unseres Internetauftritts Rechtsverstöße bemerken, bitten wir Sie uns auf diese hinzuweisen. Wir werden rechtswidrige Inhalte und Links nach Kenntnisnahme unverzüglich entfernen.</p>
+  </ContentWrapper>
+</template>
+<script setup lang="ts">
+</script>
